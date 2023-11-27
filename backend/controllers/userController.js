d33b6d99b7daeb8cbde6cacb1a4ccd6360322b2b
Saipronoti Mukherjee <saipronotimukherjee27@gmail.com>
2023-11-27 09:09:24 +0530

    authorization middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,7 +70,7 @@ const allUsers = asyncHandler(async (req, res) => {
         }
         : {};
     //console.log(keyword);
-    const users = await User.find(keyword);
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
     res.send(users);
 });
 
