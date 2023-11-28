04ec3672904024b0ca85c7a231612aef73805509
Saipronoti Mukherjee <saipronotimukherjee27@gmail.com>
2023-11-28 20:18:17 +0530

    rename group updated

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, fetchChats, createGroupChat } = require("../controllers/chatControllers");
+const { accessChat, fetchChats, createGroupChat, renameGroup } = require("../controllers/chatControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").post(protect, renameGroup);
+router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").post(protect, removeFromGroup);
 // router.route("/groupadd").post(protect, addToGroup);
 
