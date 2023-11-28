9cfd10f1828214170bf919133af3634a2f240a19
Saipronoti Mukherjee <saipronotimukherjee27@gmail.com>
2023-11-28 19:55:25 +0530

    create chat api

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -71,4 +71,38 @@ const fetchChats = asyncHandler(async (req, res) => {
      }
 });
 
-module.exports = { accessChat, fetchChats };
+const createGroupChat = asyncHandler(async (req, res) => {
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please fill all the fields " });
+    } 
+    
+    var users = JSON.parse(req.body.users);
+
+    if (users.length < 2) {
+        return res
+            .status(400)
+            .send("More than 2 users are required to form a group chat");
+    }
+
+    users.push(req.user);// adding logged in user to list of users
+
+    try { //create entry to DB
+        const groupChat = await Chat.create({
+            chatName: req.body.name,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req.user,//currently logged in user
+        });
+
+        const fullGroupChat = await Chat.findOne({ _id: groupChat._id }) //sending data back in api response
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password");
+        
+        res.status(200).json(fullGroupChat);
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat };
