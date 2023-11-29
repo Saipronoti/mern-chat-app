9b1f77fa7a0ef7498c5a489fa9cec0cb4b94c797
Saipronoti Mukherjee <saipronotimukherjee27@gmail.com>
2023-11-29 19:10:44 +0530

    side drawer rendering with react context api

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import { Box } from "@chakra-ui/layout";
+import { ChatState } from '../Context/ChatProvider';
+import SideDrawer from '../components/miscellaneous/SideDrawer';
 
+//in box we add chats if user is logged in
 const ChatPage = () => {
-    const [chats, setChats] = useState([]); 
-    const fetchChats = async () => {
-        const { data } = await axios.get('/api/chat');
-        setChats(data);
-    };
-    
-    useEffect(() => {
-            fetchChats();
-    }, []);
-
+    const { user } = ChatState();
     return (
-    <div>
-        {chats.map((chat) =>(
-        <div key={chat._id }>{chat.chatName}</div>
-        ))}
-        </div>
+    <div style={{ width: "100%"}}>
+        { user && <SideDrawer/> }
+            <Box>
+                {/* {user && <MyChats/> */} 
+                {/* {user && <ChatBox/> */}
+            </Box>        
+    </div>
     );
 };
 
--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import { Container, Box, Tab,
   TabList,
   TabPanel,
@@ -8,8 +8,18 @@ import { Container, Box, Tab,
 } from "@chakra-ui/react";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
+import { useHistory } from "react-router-dom";
 
 const Homepage = () => {
+    const history = useHistory();
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("userInfo"));
+
+        if (user) { 
+            history.push("/chats");
+        }
+    }, [history]);
     return (
         <Container maxW="xl" centerContent>
             <Box
