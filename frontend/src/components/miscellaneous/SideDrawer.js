2674fab73ebd7fc56d163836ea8e0ea704213efd
Saipronoti Mukherjee <saipronotimukherjee27@gmail.com>
2023-12-01 21:43:16 +0530

    using Avatar

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,22 +1,56 @@
-import { Box } from "@chakra-ui/layout";
-import { Tooltip } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/layout";
+import { Menu, MenuButton, Tooltip } from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { Button } from "@chakra-ui/button";
+import { Avatar } from "@chakra-ui/avatar";
 import React, { useState } from 'react'
+import { ChatState } from '../../Context/ChatProvider';
 
 const SideDrawer = () => {
     const [search, setSearch] = useState("");
     const [searchResult, setSearchResult] = useState([]);
     const [loading, setLoading] = useState(false);
     const [loadingChat, setLoadingChat] = useState();
+
+     const { user } = ChatState();
     return (
         <>
-            <Box>
+            <Box
+                d="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                bg="white"
+                w="100%"
+                p="5px 10px 5px 10px"
+                borderWidth="5px"
+            >
                 <Tooltip 
                     label="Search Users to Chat" hasArrow placement="bottom-end">
                     <Button variant="ghost">
-                        <i class="fas fa-search" aria-hidden="true"></i>
+                        <i className="fas fa-search"></i>
+                        <Text d={{base:"none",md: "flex"}} px="4">
+                           Search User 
+                        </Text>
                    </Button>
                 </Tooltip>
+
+                <Text fontSize="2xl" fontFamily="Work sans">
+                Talk-a-tive
+                </Text>
+                <div>
+                    <Menu>
+                        <MenuButton p={1}>
+                            <BellIcon  fontSize="2xl" m={1} />
+                        </MenuButton>
+                        {/*<MenuList></MenuList> */}
+                    </Menu>
+                    <Menu>
+                        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                            <Avatar size='sm' cursor='pointer' name={user.name} />
+                        </MenuButton>
+                     </Menu>   
+                </div>
+
           </Box>
         </>
     )
