a3444acec70422d7533254b2f238e725457d19fc
Saipronoti Mukherjee <saipronotimukherjee27@gmail.com>
2023-12-05 17:01:09 +0530

    being able to view both sides of 1-1 chat

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ScrollableFeed from 'react-scrollable-feed'
-import { isLastMessage, isSameSender } from '../config/ChatLogics';
+import {
+  isLastMessage,
+  isSameSender,
+  isSameSenderMargin,
+  isSameUser,
+} from "../config/ChatLogics";
 import { ChatState } from '../Context/ChatProvider';
 import { Avatar, Tooltip } from '@chakra-ui/react';
 
@@ -26,7 +31,21 @@ const ScrollableChat = ({ messages }) => {
                                 src={m.sender.pic}
                             />
                         </Tooltip>
-                    )}
+                        )}
+                    <span
+                      style={{
+                        backgroundColor: `${
+                          m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
+                                }`,
+                        marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                        marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
+                        borderRadius: "20px",
+                        padding: "5px 15px",
+                        maxWidth: "75%",
+              }}
+            >
+              {m.content}
+            </span>
                 </div>
               ))}
         </ScrollableFeed>
