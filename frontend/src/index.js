dd9351f921560e645569b47bf33ab2341ea45d49
Saipronoti Mukherjee <saipronotimukherjee27@gmail.com>
2023-12-05 15:35:56 +0530

    fix for for history.push undefined msg in browser

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,12 @@ import ChatProvider from './Context/ChatProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <ChatProvider>
-    <BrowserRouter>
     <ChakraProvider>
        <App />
       </ChakraProvider>
-      </BrowserRouter>
   </ChatProvider>
+  </BrowserRouter>,
 );
 
